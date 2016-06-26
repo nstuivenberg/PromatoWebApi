b70d86c59c55508bce96fa9c0cdac4a14dd66c3d
Nick Stuivenberg <Nick Stuivenberg>
2016-06-26 16:55:21 +0200

    yolo

---
+++ PromatoWebAPI/Controllers/CirkelController.cs
@@ -0,0 +1,56 @@
+﻿using PromatoWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PromatoWebAPI.Controllers
+{
+    public class CirkelController : ApiController
+    {
+        Cirkel[] cirkels = new Cirkel[]
+        {
+            new Cirkel {CirkelSvg = "<svg>yolo</svg>" }
+        };
+
+        /// <summary>
+        /// Hier mogelijk XML of X-document in!
+        /// </summary>
+        /// <returns></returns>
+        public IHttpActionResult GetCirkel()
+        {
+            var httpCirkel = cirkels.FirstOrDefault();
+            if (httpCirkel == null)
+            {
+                return NotFound();
+            }
+            return Ok("Test: " + httpCirkel.CirkelSvg.ToString());
+        }
+
+        //POST!
+        [HttpPost]
+        public HttpResponseMessage MooieCirkel()
+        {
+            var httpCirkel = cirkels.FirstOrDefault();
+            if (httpCirkel == null)
+            {
+                var responseWrong = new HttpResponseMessage()
+                {
+                    Content = new StringContent("yolo"),
+                };
+
+                return responseWrong;
+            }
+            var response = new HttpResponseMessage()
+            {
+                Content = new StringContent(httpCirkel.CirkelSvg),
+            };
+
+
+            return response;
+
+        }
+    }
+}
---
+++ PromatoWebAPI/Models/Cirkel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PromatoWebAPI.Models
+{
+    public class Cirkel
+    {
+        public string CirkelSvg { get; set; }
+    }
+}
